e63a4190ac01a656eed3c10e673c649245ca36ba
bilal-fiaz <bilal145j@gmail.com>
2025-12-15 14:12:40 +0500

    fixed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,13 +5,14 @@ function Navbar() {
   return (
     <nav className="navbar">
       <div className="nav-logo">BILAL</div>
-
+      <div className="list-container">
       <ul className="nav-links">
         <li><a href="#">Home</a></li>
         <li><a href="#">Quotes</a></li>
         <li><a href="#">Authors</a></li>
         <li><a href="#">About</a></li>
       </ul>
+      </div>
 
       {/* <button className="nav-btn">Connect</button> */}
     </nav>
